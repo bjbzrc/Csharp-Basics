23fe6cf4aa02d6cdf00c6250257848dfc60e7ac7
Brandon Buttlar <brandonbuttlar@mail.missouri.edu>
2019-02-06 22:16:09 -0600

    Transferred project folder to git folder

---
+++ Csharp Basics/Csharp Basics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Section for declaring constants
+            const byte sample1 = 0x3A;
+            const float acceleration = 9.800;
+            const char integral = \u222b;
+            const string greeting = "Hello";
+
+            // Section for declaring variables
+            byte sample2 = 58;
+            int heartRate = 85;
+            double deposits = 135002796;
+            float mass = 14.6;
+            double distance = 129.763001;
+            bool lost = true;
+            bool expensive = true;
+            int choice = 2;
+            string name = "Karen";
+
+            Console.WriteLine("Success!");
+            Console.ReadLine();
+        }
+    }
+}
