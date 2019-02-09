2bd32f41184415eab6846def62061275fe2b14c4
Brandon Buttlar <brandonbuttlar@mail.missouri.edu>
2019-02-08 19:18:57 -0600

    Edited declarations so code could build

--- Csharp Basics/Csharp Basics/Program.cs
+++ Csharp Basics/Csharp Basics/Program.cs
@@ -12,34 +12,39 @@ namespace Csharp_Basics
         {
             // Section for declaring constants
             const byte sample1 = 0x3A;
-            const float acceleration = 9.800;
-            const char integral = \u222b;
+            const float acceleration = 9.800F;
+            const char integral = '\u222B';
             const string greeting = "Hello";
 
             // Section for declaring variables
             byte sample2 = 58;
             int heartRate = 85;
             double deposits = 135002796;
-            float mass = 14.6;
+            float mass = 14.6F;
             double distance = 129.763001;
             bool lost = true;
             bool expensive = true;
             int choice = 2;
             string name = "Karen";
 
-            // Main tasks start here with 
+            // Main tasks start here with "sample" comparison
             if (sample1 == sample2)
             {
                 Console.WriteLine("The samples are equal.");
             }
-            if (sample1 != sample2)
+            else
             {
                 Console.WriteLine("The samples are not equal.");
             }
 
+            // Comparison for "Heart rate is..." display
             if (heartRate > 40 && heartRate < 80)
             {
                 Console.WriteLine("Heart rate is normal.");
+            }
+            else
+            {
+                Console.WriteLine("Heart rate is not normal.");
             }
 
             Console.ReadKey();
